63b2142f0e7846010a5902fa5a35a15e7048dbeb
Cuevasaj <92329439+Cuevasaj@users.noreply.github.com>
2026-07-14 13:01:55 -0600

    express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,44 @@
-console.log("Hello World");
+import "dotenv/config";
+import cors from "cors";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+
+const app = express();
+
+const port = Number(process.env.PORT) || 3000;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN ?? "http://localhost:5173/",
+  }),
+);
+
+app.use(express.json());
+
+app.get("/", (_request: Request, response: Response) => {
+  response.json({
+    message: "OK",
+  });
+});
+
+app.use(
+  (
+    error: unknown,
+    _request: Request,
+    response: Response,
+    _next: NextFunction,
+  ) => {
+    console.error(error);
+
+    response.status(500).json({
+      error: "Internal server error",
+    });
+  },
+);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
