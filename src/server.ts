d4d6c4cbc471870737067524a0b2f923b56c3866
Cuevasaj <92329439+Cuevasaj@users.noreply.github.com>
2026-07-14 14:14:45 -0600

    implemented proxy

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ import express, {
   type Request,
   type Response,
 } from "express";
+import { request } from "node:http";
+import axios from "axios";
+
+const API_KEY = process.env.API_KEY;
 
 const app = express();
 
@@ -12,7 +16,7 @@ const port = Number(process.env.PORT) || 3000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN ?? "http://localhost:5173/",
+    origin: "*",
   }),
 );
 
@@ -21,6 +25,33 @@ app.use(express.json());
 app.get("/", (_request: Request, response: Response) => {
   response.json({
     message: "OK",
+  });
+});
+
+app.get("/api/search", async (request: Request, response: Response) => {
+  console.log(request.query);
+  const userInput = request.query.userSearch;
+
+  if (!userInput) {
+    response.status(400);
+    response.json("Please provide a search query");
+    return;
+  }
+
+  const params = new URLSearchParams({
+    q: userInput as string,
+    country: "us",
+  });
+
+  const res = await axios.get(
+    `https://api.pricesapi.io/api/v1/products/search?${params}`,
+    {
+      headers: { Authorization: `Bearer ${API_KEY}` },
+    },
+  );
+  response.json({
+    message: "Welcome to prices search ",
+    data: res.data,
   });
 });
 
